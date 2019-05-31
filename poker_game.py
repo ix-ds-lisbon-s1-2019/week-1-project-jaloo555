5e7e4db72dcf86b639c6ef7f85ef0eea84be59a9
Jason Chen <superj63@hotmail.com>
2019-05-31 15:28:10 +0100

    test

--- poker_game.py
+++ poker_game.py
@@ -3,7 +3,7 @@ import random
 from operator import itemgetter
 from itertools import chain
 import collections
-
+#hi
 # Global Variables
 cardNum = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'] # for printing only
 numValues = {"2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "10":10, "J":11, "Q":12, "K":13, "A":14} # for comparing values
